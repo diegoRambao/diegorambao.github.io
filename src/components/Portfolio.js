89641d69caa1c0c1372f5ba16ff5f89d806ff11f
diego <diegoandrestrom04@gmail.com>
2020-06-29 10:36:31 -0500

    Se realizo el diseño en css de la seccion portafolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import Project from './Project';
 
+import '../assets/css/Portfolio.css';
+import dashboard1 from '../assets/img/dashboard.jpg';
+
 const Portfolio = () => {
   return (
     <section className='porfolio'>
-      <div className='title'>
-        <h2 className='title__text'>Portafolio</h2>
-      </div>
+      <div className='container'>
+        <div className='title title-center'>
+          <h2 className='title__text'>Portafolio</h2>
+        </div>
 
-      <div className='porfolio__container-card'>
-        <Project
-          name='Sistema de facturacion'
-          description='El sistema de facturacion gestiona todos los procesos invulucrados en una empresa como la compra y venta de productos'
-          img='src//img'
-        />
-        <Project
-          name='Sistema de facturacion'
-          description='El sistema de facturacion gestiona todos los procesos invulucrados en una empresa como la compra y venta de productos'
-          img='src//img'
-        />
-        <Project
-          name='Sistema de facturacion'
-          description='El sistema de facturacion gestiona todos los procesos invulucrados en una empresa como la compra y venta de productos'
-          img='src//img'
-        />
+        <div className='porfolio__container-card'>
+          <Project
+            name='Sistema de facturacion'
+            description='El sistema de facturacion gestiona todos los procesos invulucrados en una empresa como la compra y venta de productos'
+            img={dashboard1}
+          />
+          <Project
+            name='Sistema de facturacion'
+            description='El sistema de facturacion gestiona todos los procesos invulucrados en una empresa como la compra y venta de productos'
+            img={dashboard1}
+          />
+          <Project
+            name='Sistema de facturacion'
+            description='El sistema de facturacion gestiona todos los procesos invulucrados en una empresa como la compra y venta de productos'
+            img={dashboard1}
+          />
+        </div>
       </div>
     </section>
   );
