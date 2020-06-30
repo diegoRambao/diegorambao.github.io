ac37fc04b380da4e0cdb5295b9a9e2f27a44cf05
diego <diegoandrestrom04@gmail.com>
2020-06-29 19:39:38 -0500

    Updates

---
+++ precache-manifest.ea88fa9bcbc3648de07f09d6d0da0b9b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3caeebcf966b4d783c2b05eea3a67394",
+    "url": "/diegorambao.github.io/index.html"
+  },
+  {
+    "revision": "aa5b4d6724828242ef93",
+    "url": "/diegorambao.github.io/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "c029c4b6372e2ac7545a",
+    "url": "/diegorambao.github.io/static/css/main.b0dae494.chunk.css"
+  },
+  {
+    "revision": "aa5b4d6724828242ef93",
+    "url": "/diegorambao.github.io/static/js/2.c07840bb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/diegorambao.github.io/static/js/2.c07840bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c029c4b6372e2ac7545a",
+    "url": "/diegorambao.github.io/static/js/main.d830c420.chunk.js"
+  },
+  {
+    "revision": "30ae0acf4452e45b8eca",
+    "url": "/diegorambao.github.io/static/js/runtime-main.984b8178.js"
+  },
+  {
+    "revision": "8784cb962b0593b5efb51100b12ec2ed",
+    "url": "/diegorambao.github.io/static/media/fondo.8784cb96.jpg"
+  },
+  {
+    "revision": "d50682a66dbb6a9c752b4720cdb138cb",
+    "url": "/diegorambao.github.io/static/media/project1.d50682a6.jpeg"
+  },
+  {
+    "revision": "dd525eb1df63a8d9a4dc1ce0ac7b0df4",
+    "url": "/diegorambao.github.io/static/media/project2.dd525eb1.png"
+  }
+]);
